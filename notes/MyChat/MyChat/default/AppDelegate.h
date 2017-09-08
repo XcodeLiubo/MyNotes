0e73574cc98b01b996ff8d056410bf990ca5dee1
liubo <liubo@163.com>
2017-09-08 21:22:25 +0800

    初始化mychat项目

---
+++ notes/MyChat/MyChat/default/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyChat
+//
+//  Created by   LiuBo on 2017/9/8.
+//  Copyright © 2017年 LiuBo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
